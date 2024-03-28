ab225fd439c6159f7bfbd8bd08a818a44e33e5d7
= <=>
2024-03-28 13:07:42 +0700

    Trial Non Controller

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express           = require("express")
 const { PrismaClient }  = require("@prisma/client")
 const bodyParser        = require("body-parser")
 const cors              = require("cors")
-const compresssion      = require("compression")
+// const compresssion      = require("compression")
 
 // ROUTES IMPORT
 const apitest           = require("./app/routes/apitest.routes")
-const keyword           = require("./app/routes/keyword.routes")
-const skpi              = require("./app/routes/skpi.routes")
-const users             = require("./app/routes/user.routes")
+// const keyword           = require("./app/routes/keyword.routes")
+// const skpi              = require("./app/routes/skpi.routes")
+// const users             = require("./app/routes/user.routes")
 
 // APP CONFIGURATION
 const app               = express()
@@ -24,13 +24,13 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(compresssion)
+// app.use(compresssion)
 
 // ROUTING
 app.use("/test", apitest)
-app.use("/keyword", keyword)
-app.use("/skpi", skpi)
-app.use("/users", users)
+// app.use("/keyword", keyword)
+// app.use("/skpi", skpi)
+// app.use("/users", users)
 
 // APP PORT
 app.listen(5000, () => {
